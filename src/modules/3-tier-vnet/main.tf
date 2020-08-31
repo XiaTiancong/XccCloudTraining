eec96695fed26358d96f48eeb30e5a8c5631f213
Tiancong Xia <txia@MacBook-Pro.local>
2020-08-31 14:56:02 +0200

    subnet2

--- src/modules/3-tier-vnet/main.tf
+++ src/modules/3-tier-vnet/main.tf
@@ -22,3 +22,19 @@ resource "azurerm_subnet" "subnet1" {
     }
   }
 }
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = var.group
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+
+  delegation {
+    name = "acctestdelegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
