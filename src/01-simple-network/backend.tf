e81c6a5d4a7508db17389ba140fc6415f73522e1
Tiancong Xia <txia@MacBook-Pro.local>
2020-08-31 11:19:54 +0200

    terraform

--- src/01-simple-network/backend.tf
+++ src/01-simple-network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-network-terraform-backends-tiancong"
-    storage_account_name = "terraformbackends012"
+    storage_account_name = "terraformbackends01tiancong"
     container_name       = "tfstates-network"
     key                  = "01-simple-network.tfstate"
   }
