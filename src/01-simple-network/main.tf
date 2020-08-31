d609ec452e646f16e8847b27066c899375d18586
Tiancong Xia <txia@MacBook-Pro.local>
2020-08-31 14:38:00 +0200

    added module

--- src/01-simple-network/main.tf
+++ src/01-simple-network/main.tf
@@ -4,10 +4,18 @@ resource "azurerm_resource_group" "main" {
   location = "West Europe"
 }
 
-# Create a virtual network in the production-resources resource group
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-simle"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  address_space       = ["10.0.0.0/16"]
+
+module "vnet" {
+    source = "../modules/3-tier-vnet"
+    group = azurerm_resource_group.main.name
+    location = azurerm_resource_group.main.location
+    address_space = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "api" {
+  name                 = "subnet-api"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = module.vnet.vnet_name
+  address_prefixes     = ["10.0.1.0/24"]
 }
